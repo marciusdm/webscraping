c863917cda10522b60134bd846a1b5c31df0bb6b
marciusdm <marciusdm@gmail.com>
2024-06-24 15:52:11 -0300

    Adding english comments

--- scrapy_billboard/billboard/billboard/pipelines.py
+++ scrapy_billboard/billboard/billboard/pipelines.py
@@ -9,7 +9,8 @@ from itemadapter import ItemAdapter
 from billboard import Helper
 
 
-# O método process_item desta classe é invocado, cda vez que o Scrapy processa uma linha
+# en. process_item will be invoked each time a line from songs table is processed by Scrapy
+# pt. O método process_item desta classe é invocado, cda vez que o Scrapy processa uma linha
 # da tabela de músicas
 class BillboardPipeline:
     last_song = ""
@@ -18,13 +19,15 @@ class BillboardPipeline:
     last_sequential = ""
 
     def _fill_last_song(self, adapter):
-        # Quando se trata da primeira ocorrência de uma música,
-        # é salva em uma variável
+        # en. When it is the first occurrence of a song, it is saved in a variable
+        # pt. Quando se trata da primeira ocorrência de uma música, é salva em uma variável
         if 'song' in adapter.keys():
             self.last_song = adapter["song"]
-        # quando a informação da música, pego da última ocorrência
+        # en. when the song information is taken from the last occurrence
+        # pt. quando a informação da música, pego da última ocorrência
         else:
             adapter["song"] = self.last_song
+
     def _fill_last_artist(self, adapter):
         #####################################################################################
         # en. on item loader, artist  was set as the fourth <td> eleent of the line.        #
@@ -44,7 +47,11 @@ class BillboardPipeline:
             adapter["artist"] = self.last_artist
 
     def _treat_date_and_sequential(self, adapter):
-        # elimina as tags HTML e aspas
+        # en. eliminates HTML tags and quotes
+        # the load_xxx methods always return a list, even when only
+        #  one element is returned, but then I only get the first item from the list
+        ##############################################################################
+        # pt. elimina as tags HTML e aspas
         # os métodos load_xxx sempre retornam uma lista, mesmo quando apenas
         # um elemento é retornado, mas aí eu pego somente o primeiro item da lista
         adapter["date"] = Helper.strip_html_and_quotes(adapter["date"][0])
@@ -52,34 +59,53 @@ class BillboardPipeline:
             adapter["sequential_no"] = Helper.strip_html_and_quotes(
                 adapter["sequential_no"][0])
         if Helper.is_note_or_empty(adapter["date"]):
-            # adapter["date"] é sempre a segunda célula
+            # en. adapter["date"] is always the second cell
+            # but, if the line is incomplete,the date is in the first
+            # so you need to read the date from adapter
+            #################################################################
+            # pt. adapter["date"] é sempre a segunda célula
             # mas, se a linha está incompleta, a data está na primeira
             # daí é preciso ler a data de adapter
             adapter['date'] = adapter["sequential_no"]
             adapter["sequential_no"] = self.last_sequential
         elif 'sequential_no' in adapter.keys():
-            # armazeno o valor atual do número sequencial
+            # en. store the current value of the sequential number ...
+            # pt. armazeno o valor atual do número sequencial ...
             self.last_sequential = adapter["sequential_no"]
         else:
-            # para retorná-lo, quando a informação estiver ausente.
+            # en ... to return it when the information is missing.This occurs from 2012
+            # pt ... para retorná-lo, quando a informação estiver ausente.
             # Isto ocorre a partir de 2012
             adapter["sequential_no"] = self.last_sequential
         adapter["sequential_no"] = adapter["sequential_no"]
 
     def _treat_artist_and_song(self, adapter):
-        # elimina-se as tags HTML e aspas, e o caracter † presente quando
+        # en. HTML tags, quotation marks and the character † present when
+        # the song is 'song of the year' are stripped off
+        #########################################################################
+        # pt. elimina-se as tags HTML e aspas, e o caracter † presente quando
         # a música é a 'música do ano'
         adapter["song"] = Helper.strip_html_and_quotes(adapter["song"][0])
         adapter["song"] = Helper.strip_song_of_the_year_mark(adapter["song"])
         adapter["artist"] = Helper.strip_html_and_quotes(adapter["artist"][0])
 
-    # converte uma data em inglês para formato dd/mm/aaaa. O ano é lido de
-    # adapter["year"]
+    # en. converts an English date to dd/mm/yyyy format. The year is read from adapter["year"]
+    # e.g.: February 18 to 18/02
+    ##################################################################################
+    # pt. converte uma data em inglês para formato dd/mm/aaaa. O ano é lido de adapter["year"]
     # ex: February 18 para 18/02
     def _convert_date(self, adapter):
         adapter["date"] = Helper.convert_english_date(adapter["date"], adapter["year"])
 
-    # Para cada linha lida, é passado um objeto item para o 'process_item
+    # en. For each line read, an item object is passed to the 'process_item
+    # which, when encapsulated by the ItemAdapter, has the following data:
+    # adapter["sequential_no"]: first cell
+    # adapter["date"]: second cell
+    # adapter["song"]: third cell
+    # adapter["artist"]: fourth cell
+    # adapter["year"]: read directly from a variable in the spider's 'parse' method
+    #####################################################################################
+    # pt. Para cada linha lida, é passado um objeto item para o 'process_item
     # que ao ser encapsulado pelo ItemAdapter possui os seguintes dados:
     # adapter["sequential_no"]: primeira célula
     # adapter["date"]: segunda célula
@@ -94,5 +120,4 @@ class BillboardPipeline:
         self._treat_artist_and_song(adapter)
         self._treat_date_and_sequential(adapter)
         self._convert_date(adapter)
-
         return item
--- scrapy_billboard/billboard/billboard/spiders/hot100.py
+++ scrapy_billboard/billboard/billboard/spiders/hot100.py
@@ -13,16 +13,24 @@ class Hot100Spider(scrapy.Spider):
         "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number_ones_of_2012",
  ]
 
-    def parse(self, response):
+    def parse(self, response,  **kwargs):
+        # 3 seconds pause to avoid server overloading
+        # pausa de 3 segundos para não sobrecarregar o servidor
         sleep(3)
+        # en. extract the year from the page title
+        # pt. extrai o o ano a partir do título da página
         year = int(response.css("h1::text").getall()[1][-4:])
-        # Os artigos referentes aos anos abaixo, a tabela com as relação de músicas em 1º lugar na Billboard
+        # en. The articles referring to the years below, the table with the song list in first place on Billboard
+        # is third and not second on the page
+        ###################################################################################################
+        # pt. Os artigos referentes aos anos abaixo, a tabela com as relação de músicas em 1º lugar na Billboard
         # é a terceira e não a segunda da página
         years_with_warning = [1966, 1967, 1968, 1969, 1970, 1971, 1974, 1975, 1997]
         chart_history = response.xpath("//table[3]/tbody/tr") if year in years_with_warning \
             else response.xpath("//table[2]/tbody/tr")
 
-        # ignore first line
+        # en. ignore first line
+        # pt. igrnora primeira linha
         for week_chart in chart_history[1:]:
             loader = ItemLoader(item=BillboardItem(), selector=week_chart)
             loader.add_css("sequential_no", "td:nth_child(1)")
